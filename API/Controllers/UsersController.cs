e65083ea6e0f3418ba2de25f5b29de2291648db1
szalai_geri <szerg97@gmail.com>
2020-11-04 21:21:09 +0100

    end of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -19,12 +20,14 @@ namespace API.Controllers
             this.context = context;
         }
 
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await context.Users.ToListAsync();
         }
 
+        [Authorize]
         [HttpGet("{uid}")]
         public async Task<ActionResult<AppUser>> GetUser(string uid)
         {
